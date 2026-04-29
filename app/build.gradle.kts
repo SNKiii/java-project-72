d0ce1ce724635a2fd8cf891ce318cf65efbbac6d
SANky <ggsanky@gmail.com>
2026-04-29 20:44:16 +0300

    add build.gradle.kts, settings.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks.register("stage") {
+    dependsOn("clean", "shadowJar")
+}
