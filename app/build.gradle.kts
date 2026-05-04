2a22a871f8c7b78798f2eef4ad95f544b54c13e5
SANky <ggsanky@gmail.com>
2026-05-04 23:49:35 +0300

    add postaql for build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.30")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.postgresql:postgresql:42.7.3")
 }
 
 tasks.test {
